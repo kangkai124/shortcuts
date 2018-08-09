9f24dbaf71427fa38d2df3e89dac9daf1f19fb4d
kkstrive <kkstrive0124@gmail.com>
2018-08-09 18:30:05 +0800

    rm coverage

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // const host = 'http://localhost:5757'
-const host = 'http://10.1.50.100:5757'
-// const host = 'https://acfqwj85.qcloud.la'
+// const host = 'http://10.1.50.100:5757'
+const host = 'https://acfqwj85.qcloud.la'
 
 const config = {
   host,
